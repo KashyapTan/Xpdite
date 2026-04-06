134d109486d5d56b307de910269188b31c6c3bd8
kashyaptan <kashyapt.business@gmail.com>
2026-04-06 09:54:39 -0400

    refactor: added refresh icon to app icons

--- src/ui/components/icons/AppIcons.tsx
+++ src/ui/components/icons/AppIcons.tsx
@@ -191,3 +191,12 @@ export function ClipboardListIcon(props: AppIconProps) {
     </BaseIcon>
   );
 }
+
+export function RotateCcwIcon(props: AppIconProps) {
+  return (
+    <BaseIcon {...props}>
+      <path d="M3 12a9 9 0 1 0 9-9 9.75 9.75 0 0 0-6.74 2.74L3 8" />
+      <path d="M3 3v5h5" />
+    </BaseIcon>
+  );
+}
